1e90c4d9ab4772025a8f3957ee8045da5776fd96
oskargramnielsen <46709766+oskargramnielsen@users.noreply.github.com>
2023-05-03 19:44:02 +0200

    Add files via upload

--- animation1.js
+++ animation1.js
@@ -1,65 +1,65 @@
 async function init() {
 
-    const node = document.querySelector("#type-text");
-    const header = document.querySelector("header");
-    header.style.opacity = "0";
-    header.style.transform = "translate(0px, -150px)";
+  const node = document.querySelector("#type-text");
+  const header = document.querySelector("header");
+  header.style.opacity = "0";
+  header.style.transform = "translate(0px, -150px)";
 
-    await new Promise(resolve => setTimeout(resolve, 1000));
+  await new Promise(resolve => setTimeout(resolve, 1000));
 
+  node.innerText = "";
 
-    node.innerText = "";
-  
-    while (true) {
-      await node.type('Hi. I’m Oskar.');
-      await new Promise(resolve => setTimeout(resolve, 700));
-      await node.delete('Hi. I’m Oskar.');
-      await node.type('An architectural engineer.');
-      await new Promise(resolve => setTimeout(resolve, 700));
-      await node.delete('An architectural engineer.');
-      await node.type('Welcome to my website. ');
-      
-      
-    const header = document.querySelector("header");
-    
+  while (true) {
+    await node.type('Hi. I’m Oskar.');
+    await new Promise(resolve => setTimeout(resolve, 700));
+    await node.delete('Hi. I’m Oskar.');
+    await node.type('An architectural engineer.');
+    await new Promise(resolve => setTimeout(resolve, 700));
+    await node.delete('An architectural engineer.');
+    await node.type('Welcome to my website. ');
     
     header.style.opacity = "1";
     header.style.transform = "translate(0px, 0px)";
     header.style.transition = "all 1.7s ease";
-
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    await sleep(5000);
     await node.delete('Welcome to my website. ');
-    await new Promise(resolve => setTimeout(resolve, 2000));
-  
+    await sleep(2000);
+  }
+}
+
+const sleep = (time) => {
+  return new Promise(resolve => {
+    const start = Date.now();
+    const interval = setInterval(() => {
+      if (Date.now() - start >= time) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 10);
+  });
+}
+
+class TypeAsync extends HTMLSpanElement {
+  get typeInterval() {
+    const randomMs = 100 * Math.random();
+    return randomMs < 50 ? 10 : randomMs;
+  }
+
+  async type(text) {
+    for (let character of text) {
+      this.innerText += character;
+      await sleep(this.typeInterval);
     }
   }
-  
-  // Source code 🚩
-  
-  const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
-  
-  class TypeAsync extends HTMLSpanElement {
-    get typeInterval() {
-      const randomMs = 100 * Math.random();
-      return randomMs < 50 ? 10 : randomMs;
-    }
-  
-    async type(text) {
-      for (let character of text) {
-        this.innerText += character;
-        await sleep(this.typeInterval);
-      }
-    }
-  
-    async delete(text) {
-      for (let character of text) {
-        this.innerText = this.innerText.slice(0, this.innerText.length - 1);
-        await sleep(this.typeInterval);
-      }
+
+  async delete(text) {
+    for (let character of text) {
+      this.innerText = this.innerText.slice(0, this.innerText.length - 1);
+      await sleep(this.typeInterval);
     }
   }
-  
-  customElements.define("type-async", TypeAsync, { extends: "span" });
-  
-  init();
-  
+}
+
+customElements.define("type-async", TypeAsync, { extends: "span" });
+
+init();
